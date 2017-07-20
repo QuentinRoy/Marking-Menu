2895f30f18fef0cedb056e7606950947ca04c607
Quentin Roy <quentin@quentinroy.fr>
2017-07-20 23:20:28 +0700

    feat(model): Support for children of items.

--- src/model.js
+++ src/model.js
@@ -1,36 +1,52 @@
 import { deltaAngle } from './utils';
 
+const getAngleRange = items => (items.length > 4 ? 45 : 90);
+
+// Create the model item from a list of items.
+const createModelItems = (items, idPrefix = undefined) => {
+  // Calculate the angle range for each items.
+  const angleRange = getAngleRange(items);
+  // Create the frozen list of model items.
+  return Object.freeze(
+    items.map((item, i) => {
+      // Standard item id.
+      const stdId = idPrefix ? [idPrefix, i].join('-') : i.toString();
+      // Create the item without the children property.
+      const base = {
+        name: typeof item === 'string' ? item : item.name,
+        angle: i * angleRange,
+        id: item.id == null ? stdId : item.id
+      };
+      // Add the children property if the item has children.
+      if (item.children) {
+        base.children = createModelItems(item.children, stdId);
+      }
+      return Object.freeze(base);
+    })
+  );
+};
+
 /**
  * Create the marking menu model.
  *
- * @param {List<String>} itemList the list of item names.
+ * @param {List<String|{name,children}>} itemList the list of item names.
  * @return {{items, get, getNearest}}
  */
-const createModel = itemNames => {
-  const itemGap = itemNames.length > 4 ? 45 : 90;
-
-  const itemList = itemNames.map((itemName, i) =>
-    Object.freeze({
-      name: itemName,
-      angle: i * itemGap,
-      id: i
-    })
+const createModel = itemList => {
+  const modelItemList = createModelItems(itemList);
+  const modelItems = Object.freeze(
+    Object.assign(...modelItemList.map(it => ({ [it.id]: it })))
   );
-
-  const items = Object.freeze(
-    Object.assign(...itemList.map(it => ({ [it.id]: it })))
-  );
-
   return {
-    items,
+    items: modelItems,
     get(itemId) {
-      return items[itemId];
+      return modelItems[itemId];
     },
     getByName(itemName) {
-      return itemList.find(it => it.name === itemName);
+      return modelItemList.find(it => it.name === itemName);
     },
     getNearest(angle) {
-      return itemList.reduce((a, b) => {
+      return modelItemList.reduce((a, b) => {
         const deltaA = Math.abs(deltaAngle(a.angle, angle));
         const deltaB = Math.abs(deltaAngle(b.angle, angle));
         return deltaA > deltaB ? b : a;
