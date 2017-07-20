ff529f2ef4ae713189c1cddc58d5c61c530dbd8f
Quentin Roy <quentin@quentinroy.fr>
2017-07-20 22:15:21 +0700

    feat:  Selection notifications.

--- src/engine.js
+++ src/engine.js
@@ -1,21 +1,39 @@
 import { Observable } from 'rxjs';
 /**
  * Create the marking menu controller.
- * @param {Observable} drag$ higher order observable on drag manipulation.
+ * @param {{items, get, getNearest}} model
+ * @param {Observable} drag$ higher order observable on drag manipulations.
  */
-const createEngine = angleDrag$ =>
-  angleDrag$.exhaustMap(o =>
-    Observable.concat(
-      o.first().map(e => ({
-        type: 'open',
-        position: [e.center.clientX, e.center.clientY]
-      })),
-      o.map(e => ({
-        type: 'move',
-        alpha: e.alpha
-      })),
-      Observable.from([{ type: 'close' }])
-    )
-  );
+const createEngine = (model, angleDrag$) =>
+  angleDrag$.exhaustMap(o_ => {
+    const o = o_.share();
+    const start$ = o.first().map(e => ({
+      type: 'open',
+      position: [e.center.clientX, e.center.clientY]
+    }));
+    const moveAndChange$ = o
+      .skip(1)
+      .map(e => ({
+        alpha: e.alpha,
+        position: e.position,
+        active: model.getNearest(e.alpha)
+      }))
+      .scan(
+        (last, current) =>
+          Object.assign(
+            {
+              type: last && last.active === current.active ? 'move' : 'change'
+            },
+            current
+          ),
+        null
+      )
+      .share();
+    const end$ = moveAndChange$.startWith({}).last().map(e => ({
+      type: e.active ? 'select' : 'cancel',
+      selection: e.active
+    }));
+    return Observable.merge(start$, moveAndChange$, end$);
+  });
 
 export default createEngine;
--- src/index.js
+++ src/index.js
@@ -6,41 +6,49 @@ import { watchAngleDrag } from './watch-drag';
 export default (items, parentDOM) => {
   const model = createModel(items);
   // Create the engine.
-  const menuEvent$ = createEngine(watchAngleDrag(parentDOM));
+  const engineNotif$ = createEngine(model, watchAngleDrag(parentDOM)).share();
   // Open the menu in function of engine events.
   let menu = null;
-  menuEvent$.subscribe({
-    next(evt) {
-      try {
-        switch (evt.type) {
-          case 'open': {
-            const cbr = parentDOM.getBoundingClientRect();
-            menu = createMenu(model, parentDOM, [
-              evt.position[0] - cbr.left,
-              evt.position[1] - cbr.top
-            ]);
-            break;
+  const action$ = engineNotif$
+    .do({
+      next(notif) {
+        try {
+          switch (notif.type) {
+            case 'open': {
+              const cbr = parentDOM.getBoundingClientRect();
+              menu = createMenu(model, parentDOM, [
+                notif.position[0] - cbr.left,
+                notif.position[1] - cbr.top
+              ]);
+              break;
+            }
+            case 'change': {
+              menu.setActive(notif.active.id);
+              break;
+            }
+            case 'select':
+            case 'cancel':
+              menu.remove();
+              menu = null;
+              break;
+            case 'move':
+              break;
+            default:
+              throw new Error(`Invalid engine type: ${notif.type}`);
           }
-          case 'move': {
-            const nearest = model.getNearest(evt.alpha);
-            menu.setActive(nearest.id);
-            break;
-          }
-          case 'close':
-            menu.remove();
-            menu = null;
-            break;
-          default:
-            throw new Error(`Invalid engine type: ${evt.type}`);
+        } catch (err) {
+          console.error(err);
+          throw err;
         }
-      } catch (err) {
+      },
+      error(err) {
         console.error(err);
         throw err;
       }
-    },
-    error(err) {
-      console.error(err);
-      throw err;
-    }
-  });
+    })
+    .share();
+  action$.subscribe();
+  return action$
+    .filter(notif => notif.type === 'select')
+    .pluck('selection', 'name');
 };
