b9586251490d25cd4c9ca291c5c50ec4e749310d
Quentin Roy <quentin@quentinroy.fr>
2017-07-29 12:44:56 +0800

    chore: Add banner to built file.

--- build-config/rollup.config.js
+++ build-config/rollup.config.js
@@ -5,6 +5,20 @@ import pug from 'rollup-plugin-pug';
 import babel from 'rollup-plugin-babel';
 import sass from 'rollup-plugin-sass';
 import sassTrigo from './sass-trigo';
+import { version } from '../package.json'
+
+const banner = `/*!
+ * Marking Menu Javascript Library v${version}
+ * https://github.com/QuentinRoy/Marking-Menu
+ *
+ * Released under the MIT license.
+ * https://raw.githubusercontent.com/QuentinRoy/Marking-Menu/master/LICENSE
+ *
+ * Marking Menus may be patented independently from this software.
+ *
+ * Date: ${new Date().toUTCString()}
+ */
+`
 
 export default {
   entry: 'src/index.js',
@@ -27,6 +41,7 @@ export default {
   globals: {
     'rxjs': 'Rx'
   },
+  banner,
   dest: './marking-menu.js',
   sourceMap: true
 };
