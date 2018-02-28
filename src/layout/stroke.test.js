588f136f476fa1bd3af1abfc4111679bc5e86290
Quentin Roy <quentin@quentinroy.fr>
2018-02-28 03:27:21 +0100

    test: add missing stroke test

--- src/layout/stroke.test.js
+++ src/layout/stroke.test.js
@@ -23,26 +23,39 @@ const createMockContext = () =>
     }
   );
 
-beforeEach(() => {
-  document.createElement = jest.fn((t, ...args) => {
-    const elt = docCreateElement.call(document, t, ...args);
-    if (t === 'canvas') {
-      const context = createMockContext();
-      elt.getContext = () => context;
-    }
-    return elt;
-  });
-});
-
-afterEach(() => {
-  document.createElement = docCreateElement;
-});
-
 describe('stroke', () => {
-  it('creates its DOM and set up the canvas scale', () => {
-    const div = document.createElement('div');
+  let strokeCanvas;
+  let div;
+  let mockContext;
+
+  beforeEach(() => {
+    document.createElement = jest.fn((t, ...args) => {
+      const elt = docCreateElement.call(document, t, ...args);
+      if (t === 'canvas') {
+        const context = createMockContext();
+        elt.getContext = () => context;
+      }
+      return elt;
+    });
+
+    div = document.createElement('div');
     div.getBoundingClientRect = () => ({ width: 50, height: 60 });
-    stroke(div, { doc: document, ptSize: 1 / 4 });
+    strokeCanvas = stroke(div, {
+      doc: document,
+      ptSize: 1 / 4,
+      pointRadius: 100,
+      pointColor: 'mockPointColor',
+      lineWidth: 7,
+      lineColor: 'mockLineColor'
+    });
+    mockContext = div.querySelector('canvas').getContext();
+  });
+
+  afterEach(() => {
+    document.createElement = docCreateElement;
+  });
+
+  it('creates its DOM and set up the canvas scale', () => {
     expect(div).toMatchSnapshot();
     expect(div.querySelector('canvas').getContext().mock.methodCalls).toEqual([
       { method: 'scale', args: [4, 4] }
@@ -50,23 +63,11 @@ describe('stroke', () => {
   });
 
   it('can be removed', () => {
-    const div = document.createElement('div');
-    div.getBoundingClientRect = () => ({ width: 50, height: 60 });
-    const strokeCanvas = stroke(div, { doc: document, ptSize: 1 / 4 });
     strokeCanvas.remove();
     expect(div).toMatchSnapshot();
   });
 
   it('can draw points', () => {
-    const div = document.createElement('div');
-    div.getBoundingClientRect = () => ({ width: 50, height: 60 });
-    const strokeCanvas = stroke(div, {
-      doc: document,
-      ptSize: 1 / 4,
-      pointRadius: 100,
-      pointColor: 'mockPointColor'
-    });
-    const mockContext = div.querySelector('canvas').getContext();
     // Clear the method calls.
     mockContext.mock.methodCalls = [];
 
@@ -76,20 +77,18 @@ describe('stroke', () => {
   });
 
   it('can draw strokes', () => {
-    const div = document.createElement('div');
-    div.getBoundingClientRect = () => ({ width: 50, height: 60 });
-    const strokeCanvas = stroke(div, {
-      doc: document,
-      ptSize: 1 / 4,
-      lineWidth: 7,
-      lineColor: 'mockLineColor'
-    });
-    const mockContext = div.querySelector('canvas').getContext();
     // Clear the method calls.
     mockContext.mock.methodCalls = [];
 
     strokeCanvas.drawStroke([[3, 2], [2, 4], [5, 1], [9, 0]]);
     strokeCanvas.drawStroke([[10, 0], [0, 0], [1, 1], [1, 0]]);
+    expect(mockContext.mock.methodCalls).toMatchSnapshot();
+  });
+
+  it('can be cleared', () => {
+    // Clear the method calls.
+    mockContext.mock.methodCalls = [];
+    strokeCanvas.clear();
     expect(mockContext.mock.methodCalls).toMatchSnapshot();
   });
 });
